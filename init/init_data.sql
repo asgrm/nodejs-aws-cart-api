4c41fde9fd8dfc28832d5c70781872a20a4eea32
Andrii Gromakov <andriigromakov@gmail.com>
2023-12-24 12:59:35 +0200

    fix(task-8) minor fix

--- init/init_data.sql
+++ init/init_data.sql
@@ -1,6 +1,4 @@
-INSERT INTO product (id, description, price, title) VALUES
-('7567ec4b-b10c-48c5-9345-fc73c48a80aa', 'Fun Stacking Blocks', 12, 'Stacking Blocks Set'),
-('7567ec4b-b10c-48c5-9345-fc73c48a80a1', 'Interactive Robot Friend', 25, 'RoboBuddy');
+INSERT INTO product (id, description, price, title) VALUES ('7567ec4b-b10c-48c5-9345-fc73c48a80aa', 'Fun Stacking Blocks', 12, 'Stacking Blocks Set'), ('7567ec4b-b10c-48c5-9345-fc73c48a80a1', 'Interactive Robot Friend', 25, 'RoboBuddy');
 
 INSERT INTO cart(id, user_id, status) VALUES ('f45a8638-7c94-45b4-87d5-40fee1bd2f11', 'test_user_1', 'OPEN');
 
